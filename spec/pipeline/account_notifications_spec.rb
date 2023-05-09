a57548d2f9ba51cf17deb76f8b33b5ca208b7da7
Scott Gibson <sevgibson@gmail.com>
2023-05-09 12:03:43 -0400

    rubocop

--- spec/pipeline/account_notifications_spec.rb
+++ spec/pipeline/account_notifications_spec.rb
@@ -1,19 +1,20 @@
+# frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
 describe Pipeline::AccountNotification do
-  it_should_behave_like "a paginated collection"
+  it_behaves_like "a paginated collection"
 
   it "creates a notification" do
     VCR.use_cassette(:create_notification) do
-      notification = Pipeline::AccountNotification.create(text: "Testing Notification")
+      notification = described_class.create(text: "Testing Notification")
       expect(notification.id).not_to be_nil
     end
   end
 
   it "lists notifications" do
     VCR.use_cassette(:index_notifications) do
-      notifications = Pipeline::AccountNotification.all
+      notifications = described_class.all
       expect(notifications.size).to eq(9)
     end
   end
