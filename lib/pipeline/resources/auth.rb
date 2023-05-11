4da1d01e436e4f29788eb0c96e14447dc1b0db84
Scott Gibson <sevgibson@gmail.com>
2023-05-11 09:44:31 -0400

    handle either jwt or api_key

--- lib/pipeline/resources/auth.rb
+++ lib/pipeline/resources/auth.rb
@@ -3,13 +3,31 @@ module Pipeline
     include ActiveResource::Singleton
 
     def self.authenticate(email, password, mfa_code = nil, options = {})
-      add_keys(options ||= {})
+      add_keys(options)
       options.merge!(email_or_username: email, password: password, mfa_code: mfa_code)
+
       self.include_root_in_json = false
-      a = create(options)
-      Pipeline.configure { |c| c.app_key = nil; c.auth_type = :bearer; c.bearer_token = a.token }
+      auth = create(options)
       self.include_root_in_json = true
-      Pipeline::Account.account
+
+      if auth.respond_to?(:token)
+        configure_jwt_token(auth)
+        Pipeline::User.find(auth.user.id)
+      else
+        configure_api_key(auth)
+        # For some reason, this path does not return the user within a hash, so the user id is at the top level.
+        Pipeline::User.find(auth.id)
+      end
+    end
+
+    private
+
+    def self.configure_api_key(auth)
+      Pipeline.configure { |c| c.api_key = auth.api_key }
+    end
+
+    def self.configure_jwt_token(auth)
+      Pipeline.configure { |c| c.app_key = nil; c.auth_type = :bearer; c.bearer_token = auth.token }
     end
   end
 end
--- spec/pipeline/auth_spec.rb
+++ spec/pipeline/auth_spec.rb
@@ -3,10 +3,17 @@
 require "spec_helper"
 
 describe Pipeline::Auth do
-  it "authenticates a user" do
-    VCR.use_cassette(:auth_success) do
+  it "authenticates a user with jwt token" do
+    VCR.use_cassette(:auth_jwt_success) do
       a = described_class.authenticate("x@gmail.com", "abc123$$$$", nil, app_key: "010a14be40ff5deafb7de7e773b8bff0")
-      expect(a.id).to eq(65)
+      expect(a.id).to eq(451)
+    end
+  end
+
+  it "authenticates a user with jwt token" do
+    VCR.use_cassette(:auth_api_key_success) do
+      a = described_class.authenticate("x@gmail.com", "abc123$$$$", nil, app_key: "8ddaff5a21fd18070be60623e5074781")
+      expect(a.id).to eq(451)
     end
   end
 
