fbf9f47fc8910018bbfcfc25b2a5ad63896cf623
Scott Gibson <sevgibson@gmail.com>
2023-05-19 13:44:52 -0400

    handle configure_jwt_token from outside

--- lib/pipeline/resources/auth.rb
+++ lib/pipeline/resources/auth.rb
@@ -4,6 +4,8 @@ module Pipeline
   class Auth < Pipeline::Resource
     include ActiveResource::Singleton
 
+    self.collection_name = "auth"
+
     def self.authenticate(email, password, mfa_code = nil, options = {})
       add_keys(options)
       options.merge!(email_or_username: email, password: password, mfa_code: mfa_code)
@@ -23,8 +25,9 @@ module Pipeline
       Pipeline::User.find(user.id)
     end
 
-    def self.configure_api_key(auth)
+    def self.configure_api_key(auth, options = {})
       Pipeline.configure { |c| c.api_key = auth.api_key }
+      add_keys(options)
       # This path does not return the user within a hash, so the user id is at the top level.
       # It turns out that if the return hash only has one key, then it always assumes it is a "root" and removes
       # that level:
@@ -32,12 +35,13 @@ module Pipeline
       auth
     end
 
-    def self.configure_jwt_token(auth)
+    def self.configure_jwt_token(auth, options = {})
       Pipeline.configure do |c|
         c.app_key = nil
         c.auth_type = :bearer
         c.bearer_token = auth.token
       end
+      add_keys(options)
       auth.user
     end
   end
