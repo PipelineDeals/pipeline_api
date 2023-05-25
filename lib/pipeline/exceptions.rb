9c77083d34f50fae5ec19391241e475d12da11cb
Scott Gibson <sevgibson@gmail.com>
2023-05-24 20:28:17 -0400

    dont call Pipeline a module

--- lib/pipeline/exceptions.rb
+++ lib/pipeline/exceptions.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
-module Pipeline
-  module Exceptions
-    class BadRequestError < StandardError; end
-    class NotAuthorizedError < StandardError; end
-    class PermissionDeinedError < StandardError; end
-    class NotFoundError < StandardError; end
-    class NotAcceptableError < StandardError; end
-    class TooManyRequestsError < StandardError; end
-    class InternalPipelineError < StandardError; end
+module Pipeline::Exceptions
+  class BadRequestError < StandardError; end
+  class NotAuthorizedError < StandardError; end
+  class PermissionDeinedError < StandardError; end
+  class NotFoundError < StandardError; end
+  class NotAcceptableError < StandardError; end
+  class TooManyRequestsError < StandardError; end
+  class InternalPipelineError < StandardError; end
 
-    class ApiError < StandardError
-      attr_reader :code
+  class ApiError < StandardError
+    attr_reader :code
 
-      def initialize(message, code)
-        @code = code
-        super(message)
-      end
+    def initialize(message, code)
+      @code = code
+      super(message)
     end
   end
 end
