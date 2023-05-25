79529adbbc953d65c9dca148933e41c841138f2b
Scott Gibson <sevgibson@gmail.com>
2023-05-24 21:27:24 -0400

    forgot to config cov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,34 @@
 require "rubygems"
 require "vcr"
 require "pipeline"
-require "simplecov"
 
 begin
   require "pry"
 rescue LoadError
+end
+
+if ENV["TEST_REPORTER"] || ENV["CI"]
+  require "simplecov"
+  require "active_support/inflector"
+
+  SimpleCov.start "rails" do
+    enable_coverage :branch
+    coverage_criterion :branch
+
+    add_filter "/config/"
+    add_filter "/spec/"
+    add_filter "/test/"
+    add_filter "/vendor/"
+    add_filter "/db/"
+    add_filter "/db_historical/"
+
+    Dir["app/*"].each do |dir|
+      add_group File.basename(dir).humanize, dir
+    end
+
+    minimum_coverage 0
+    merge_timeout 3600
+  end
 end
 
 VCR.configure do |c|
