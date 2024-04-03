cd5fec00b669f3c3b4cc372c0d38489f19b66429
Scott Gibson <sevgibson@gmail.com>
2024-04-02 20:13:20 -0400

    use body for put

--- lib/pipeline/admin/feature.rb
+++ lib/pipeline/admin/feature.rb
@@ -10,6 +10,6 @@ class Pipeline::Admin::Feature < Pipeline::Resource
   end
 
   def add_provider(provided_by, data)
-    _put("features/#{id}", query: { provided_by:, data: })
+    _put("features/#{id}", body: { provided_by:, data: })
   end
 end
